abd3c74ee7b6ec421058b4e62a7afaac397a3c95
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-18 22:17:50 -0600

    jest tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,7 @@ import { fireEvent } from '@testing-library/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { act } from 'react-dom/test-utils'
+import renderer from 'react-test-renderer'
 //import { render, fireEvent, getByTestId } from 'react-testing-library'
 import App from './App'
 
@@ -18,20 +19,18 @@ afterEach(() => {
   container = null
 })
 
-it('Renders without crashing', () => {
+test('Renders without crashing', () => {
   act(() => {
     ReactDOM.createRoot(container).render(<App />)
   })
 })
 
-it('Funcionamiento al seleccionar 1, 2, 3 y *', () => {
+test('Funcionamiento al seleccionar 1, 2, 3 y *', () => {
   act(() => {
     ReactDOM.createRoot(container).render(<App />)
   })
   const labels = container.querySelectorAll('h1')
   const btns = container.querySelectorAll('button')
-
-  expect(labels[0].textContent).toBe('ANSWER: ')
 
   function click(element) {
     fireEvent(
@@ -52,14 +51,12 @@ it('Funcionamiento al seleccionar 1, 2, 3 y *', () => {
   expect(labels[1].textContent).toBe('VALUE: ')
 })
 
-it('Funcionamiento al seleccionar 4, 5, 6 y -', () => {
+test('Funcionamiento al seleccionar 4, 5, 6 y -', () => {
   act(() => {
     ReactDOM.createRoot(container).render(<App />)
   })
   const labels = container.querySelectorAll('h1')
   const btns = container.querySelectorAll('button')
-
-  expect(labels[0].textContent).toBe('ANSWER: ')
 
   function click(element) {
     fireEvent(
@@ -80,14 +77,12 @@ it('Funcionamiento al seleccionar 4, 5, 6 y -', () => {
   expect(labels[1].textContent).toBe('VALUE: ')
 })
 
-it('Funcionamiento al seleccionar 7, 8, 9 y +', () => {
+test('Funcionamiento al seleccionar 7, 8, 9 y +', () => {
   act(() => {
     ReactDOM.createRoot(container).render(<App />)
   })
   const labels = container.querySelectorAll('h1')
   const btns = container.querySelectorAll('button')
-
-  expect(labels[0].textContent).toBe('ANSWER: ')
 
   function click(element) {
     fireEvent(
@@ -106,4 +101,135 @@ it('Funcionamiento al seleccionar 7, 8, 9 y +', () => {
   expect(labels[1].textContent).toBe('VALUE: 789')
   click(btns[15])
   expect(labels[1].textContent).toBe('VALUE: ')
+})
+
+test('Renders correctly with snapshot', () => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Funcionamiento de toggle sign +/-', () => {
+  act(() => {
+    ReactDOM.createRoot(container).render(<App />)
+  })
+  const labels = container.querySelectorAll('h1')
+  const btns = container.querySelectorAll('button')
+
+  function click(element) {
+    fireEvent(
+      element,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+  }
+  click(btns[12])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+  click(btns[1])
+  expect(labels[1].textContent).toBe('VALUE: -7')
+  click(btns[1])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+})
+
+test('Funcionamiento = (respuesta correcta)', () => {
+  act(() => {
+    ReactDOM.createRoot(container).render(<App />)
+  })
+  const labels = container.querySelectorAll('h1')
+  const btns = container.querySelectorAll('button')
+
+  function click(element) {
+    fireEvent(
+      element,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+  }
+  click(btns[12])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+  click(btns[13])
+  expect(labels[1].textContent).toBe('VALUE: 78')
+  click(btns[14])
+  expect(labels[1].textContent).toBe('VALUE: 789')
+  click(btns[11])
+  expect(labels[1].textContent).toBe('VALUE: ')
+  click(btns[12])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+  click(btns[13])
+  expect(labels[1].textContent).toBe('VALUE: 78')
+  click(btns[19])
+  expect(labels[0].textContent).toBe('ANSWER: 711')
+})
+
+test('Funcionamiento Clear', () => {
+  act(() => {
+    ReactDOM.createRoot(container).render(<App />)
+  })
+  const labels = container.querySelectorAll('h1')
+  const btns = container.querySelectorAll('button')
+
+  function click(element) {
+    fireEvent(
+      element,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+  }
+  click(btns[12])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+  click(btns[13])
+  expect(labels[1].textContent).toBe('VALUE: 78')
+  click(btns[14])
+  expect(labels[1].textContent).toBe('VALUE: 789')
+  click(btns[11])
+  expect(labels[1].textContent).toBe('VALUE: ')
+  click(btns[12])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+  click(btns[13])
+  expect(labels[1].textContent).toBe('VALUE: 78')
+  click(btns[19])
+  expect(labels[0].textContent).toBe('ANSWER: 711')
+  click(btns[12])
+  expect(labels[1].textContent).toBe('VALUE: 7')
+  click(btns[0])
+  expect(labels[0].textContent).toBe('ANSWER: ')
+  expect(labels[1].textContent).toBe('VALUE: ')
+})
+
+test('Verify if calculation generates ERROR', () => {
+  act(() => {
+    ReactDOM.createRoot(container).render(<App />)
+  })
+  const labels = container.querySelectorAll('h1')
+  const btns = container.querySelectorAll('button')
+
+  function click(element) {
+    fireEvent(
+      element,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+  }
+  click(btns[14])
+  click(btns[14])
+  click(btns[14])
+  click(btns[14])
+  click(btns[14])
+  click(btns[14])
+  click(btns[14])
+  click(btns[7])
+  click(btns[4])
+  click(btns[16])
+  click(btns[16])
+  click(btns[16])
+  click(btns[16])
+  click(btns[19])
+  expect(labels[0].textContent).toBe('ANSWER: ERROR')
 })
